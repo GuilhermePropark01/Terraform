eba409f601d2cf9119c62ecf19bfcc5a4eb55bc9
Carlos Silva <carlosguilherme122@gmail.com>
2022-11-22 08:23:38 -0300

    Aula de remote state

---
+++ State/Criação do bucket para Remote State/bucket.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "remotestate-carlossilva"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.first_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
